ee7ac0a9dcdb934c52ac4734cc14e9d6d1217985
lavalex <lavrov.alex@gmail.com>
2026-08-20 10:04:04 +0000

    updated flow

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-// Define Prometheus Metrics alex
+// Define Prometheus Metrics
 var (
 	applesEatenCounter = promauto.NewCounter(prometheus.CounterOpts{
 		Name: "snake_apples_eaten_total",
